5dbcbcd7878a48af7fb105867bcc37f271512f17
hanshenrikjeppesen <info@frist.dk>
2018-04-06 21:46:43 +0200

    correct int to str conversion

--- 1_5_integer_conversion.py
+++ 1_5_integer_conversion.py
@@ -23,10 +23,10 @@ def convert_integer(data):
 
 while True:
     userInput = (input('\033[1;37;40mType in an Integer (0-10000) or "exit()" to quit: '))
-    if int(userInput) > 10000 or int(userInput) < 0:
-        print('The interger you have typed in is not valid, please try again')
-    elif userInput.lower() == 'exit()':
+    if userInput.lower() == 'exit()' :
         break
+    elif int(userInput) > 10000 or int(userInput) < 0:
+        print('The interger you have typed in is not valid, please try again')
     else:
         data = int(userInput)
         convert_integer(data)
