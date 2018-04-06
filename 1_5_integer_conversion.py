44d31427ef1c967cb8b8d8545837c92ca059f59a
hanshenrikjeppesen <info@frist.dk>
2018-04-06 20:29:10 +0200

    bug in input

--- 1_5_integer_conversion.py
+++ 1_5_integer_conversion.py
@@ -10,11 +10,13 @@ import socket
 
 def convert_integer():
     while True:
-        data = input('Type in an Integer (0-10000: ')
-        if data > 10000 or data < 0:
+        userInput = int(input('Type in an Integer (0-10000): '))
+        if userInput > 10000 or userInput < 0:
             print('The interger you have typed in is not valid, please try again')
         else:
+            data = userInput
             break
+
     # 32 bit conversion Network To Host Long (ntohl) and Host to Network Long (htonl)
     print("Original input: {} => \nLong host byte order: {} \nLong network byte order: {}".format(data, socket.ntohl(data), socket.htonl(data)))
 
