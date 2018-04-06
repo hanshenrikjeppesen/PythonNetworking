f0a0ed56be5f1925e809598c7ddd4a911ad27c12
hanshenrikjeppesen <info@frist.dk>
2018-04-06 20:04:11 +0200

    New script file

---
+++ 1_5_integer_conversion.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# ___________________________________________________________
+# Python Network Programming Introduction ITEK - EAAA 2018
+# Author Hans Henrik Jeppesen
+# Date: 27.03.18 Version 1.0
+# Contact: hans@eaaa.dk
+# ___________________________________________________________
+
+import socket
+
+def convert_integer():
+    while True:
+        data = input('Type in an Integer (0-10000: ')
+        if data > 10000 or data < 0:
+            print('The interger you have typed in is not valid, please try again')
+        else:
+            break
+    # 32 bit conversion Network To Host Long (ntohl) and Host to Network Long (htonl)
+    print("Original input: {} => \nLong host byte order: {} \nLong network byte order: {}".format(data, socket.ntohl(data), socket.htonl(data)))
+
+    # 16 bit conversion  Network To Host Short (ntohs) and Host to Network Short (htons)
+
+    print("Original input: {} => \nShort host byte order: {} \nShort network byte order: {}".format(data, socket.ntohs(data), socket.htons(data)))
+
+    if __name__ == '__main__':
+        convert_integer()
